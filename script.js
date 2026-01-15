373b7ef530639dd99515fc25c0aaf89f3681ee00
MUHAMMADULLAH1991 <muhammadullahsaify859658@gmail.com>
2026-01-15 20:48:31 +0600

    Problem solving related if-else

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+let digit;
+
+digit = 0;
+
+if (digit > 0){
+    console.log("The number is positive");
+}
+else if(digit < 0){
+    console.log("The number is nagative");
+}
+else{
+    console.log("The number is 0");
+}
+
+
+let number;
+
+number = 20003;
+
+if(number%2 == 0){
+    console.log("This is even number");
+}
+
+else{
+    console.log("This is odd number");
+}
+
+
+let mark;
+
+mark = 955;
+
+if(mark < 0 || mark > 100){
+    console.log("Invalid Mark");
+}
+else if(mark >= 80){
+    console.log("The grade is A+");
+}
+else if(mark >= 70){
+    console.log("The grade is A");
+}
+else if(mark >= 60){
+    console.log("The grade is A-");
+}
+else if(mark >= 50){
+    console.log("The grade is B");
+}
+else if(mark >= 40){
+    console.log("The is grade is c");
+}
+else if(mark >= 33){
+    console.log("The grade is D");
+}
+else{
+    console.log("The grade is F")
+}
